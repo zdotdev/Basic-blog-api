b4118fcd8f9c3194b3923f51c1bfccfc936cc4cf
Zed0825 <www.ragaszedrick25@gmail.com>
2024-02-10 12:48:42 +0800

    update delete route

--- routes/blog-routes.js
+++ routes/blog-routes.js
@@ -7,7 +7,7 @@ blogRouter.get("/", getAllBlogs)
 blogRouter.post("/add", addBlog)
 blogRouter.put("/update/:id", updateBlog)
 blogRouter.get("/:id", getByID)
-blogRouter.delete("/:id", deleteBlog)
+blogRouter.delete("/delete/:id", deleteBlog)
 blogRouter.get("/user/:id", getByUserID)
 
 export default blogRouter
